b4d98faf0570cb32dbdb77820e9a3e1922aa0d00
Marion <marionbailleux07@gmail.com>
2023-11-08 12:03:13 +0100

    affichage trips found

--- script.js
+++ script.js
@@ -1,24 +1,40 @@
 let searchBtn = document.getElementById("searchBtn");
 
 function getTrips() {
-  let departure = document.querySelector(".departureField").value;
-  let arrival = document.querySelector(".arrivalField").value;
-  let date = document.querySelector(".dateField").value;
+  let departure = document.querySelector("#departureInput").value;
+  let arrival = document.querySelector("#arrivalInput").value;
+  let date = document.querySelector("#date").value;
   const tripsContainer = document.querySelector(".tripsContainer");
 
   fetch(`http://localhost:3000/trips/${departure}/${arrival}/${date}`)
     .then((res) => res.json())
     .then((data) => {
       if (data.length > 0) {
+        tripsContainer.innerHTML = "";
         for (let trip of data) {
           tripsContainer.innerHTML += `
-              <div>
-                  <span class="departure">${trip.departure}</span> > <span class="arrival">${trip.arrival}</span>
-                  <span class="price">${trip.price}</span>
-                  <div id="bookBtn">Book</div>
-              </div>
+          <div class="trip">
+          <div class="fieldsContainer">
+            <span class="departure-arrival">${trip.departure} > ${
+            trip.arrival
+          }</span>
+            <span class="hour">${new Date(trip.date)
+              .getHours()
+              .toString()
+              .padStart(2, "0")}:${new Date(trip.date)
+            .getMinutes()
+            .toString()
+            .padStart(2, "0")}</span>
+            <span class="price">${trip.price}€</span>
+          </div>
+  
+          <div class="bookBtn">Book</div>
+        </div>
               `;
         }
+      } else if (data.length == 0) {
+        tripsContainer.querySelector("img").src = "./images/notfound.png";
+        tripsContainer.querySelector("p").textContent = "No trip found";
       }
     });
 }
